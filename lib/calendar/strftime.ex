d037531c35ebaed1ec68feef189c667a4388929a
David Southard <dsouthard@wisc.edu>
2026-09-11 11:29:03 -0500

    lint file on save

--- lib/calendar/strftime.ex
+++ lib/calendar/strftime.ex
@@ -25,7 +25,7 @@ defmodule Calendar.Strftime do
   def strftime!(dt, string, lang \\ :en) do
     string
     |> parse_for_con_specs
-    |> Enum.reduce(string, fn(conv_spec, new_string) ->
+    |> Enum.reduce(string, fn conv_spec, new_string ->
       String.replace(new_string, "%#{conv_spec}", string_for_conv_spec(dt, conv_spec, lang))
     end)
   end
@@ -113,12 +113,13 @@ defmodule Calendar.Strftime do
   | date and time          | `DateTime`, `NaiveDateTime`, datetime tuple                     |
   | date and time and TZ   | `DateTime`                                                      |
   """
-  def strftime(dt, string, lang\\:en) do
+  def strftime(dt, string, lang \\ :en) do
     try do
       {:ok, strftime!(dt, string, lang)}
     rescue
       _error in [KeyError, Protocol.UndefinedError] ->
         {:error, :missing_data_for_conversion_spec}
+
       error ->
         {:error, error}
     end
@@ -126,47 +127,161 @@ defmodule Calendar.Strftime do
 
   defp parse_for_con_specs(string) do
     Regex.scan(~r/\%[a-zA-Z]/, string)
-    |> Enum.map(fn(x) -> hd(x)|>String.replace("%","")|>String.to_atom end)
+    |> Enum.map(fn x -> hd(x) |> String.replace("%", "") |> String.to_atom() end)
   end
 
   # Takes for instance a DateTime for 2014-9-6 17:10:20 and :Y and returns "2014"
-  defp string_for_conv_spec(dt, :a, lang) do weekday_abbr(dt, lang) end
-  defp string_for_conv_spec(dt, :A, lang) do weekday(dt, lang) end
-  defp string_for_conv_spec(dt, :b, lang) do month_abbr(dt, lang) end
-  defp string_for_conv_spec(dt, :h, lang) do string_for_conv_spec(dt, :b, lang) end
-  defp string_for_conv_spec(dt, :B, lang) do month(dt, lang) end
-  defp string_for_conv_spec(dt, :f, _) do micro_seconds(dt) end
-  defp string_for_conv_spec(dt, :j, _) do "#{day_number_in_year(dt)}"|>pad(3) end
-  defp string_for_conv_spec(dt, :u, _) do "#{day_of_the_week(dt)}" end
-  defp string_for_conv_spec(dt, :w, _) do "#{day_of_the_week_zero_sunday(dt)}" end
-  defp string_for_conv_spec(dt, :V, _) do "#{elem(iso_week_number(dt),1)}"|>pad end
-  defp string_for_conv_spec(dt, :G, _) do "#{elem(iso_week_number(dt),0)}"|>pad(4) end
-  defp string_for_conv_spec(dt, :g, _) do string_for_conv_spec(dt, :G, nil)|>String.slice(-2..-1) end
-  defp string_for_conv_spec(dt, :y, _) do dt=to_date(dt);"#{dt.year}" |> String.slice(-2..-1) end
-  defp string_for_conv_spec(dt, :Y, _) do dt=to_date(dt);"#{dt.year}"|>pad(4) end
-  defp string_for_conv_spec(dt, :C, _) do dt=to_date(dt);"#{(dt.year/100.0)|>trunc}" end
-  defp string_for_conv_spec(dt, :I, _) do dt=to_time(dt);"#{dt.hour|>x24h_to_12_h|>elem(0)}"|>pad end
-  defp string_for_conv_spec(dt, :l, _) do dt=to_time(dt);"#{dt.hour|>x24h_to_12_h|>elem(0)}"|>pad(2, hd ' ') end
-  defp string_for_conv_spec(dt, :P, _) do dt=to_time(dt);"#{dt.hour|>x24h_to_12_h|>elem(1)}" end
-  defp string_for_conv_spec(dt, :p, _) do dt=to_time(dt);"#{dt.hour|>x24h_to_12_h|>elem(1)}"|>String.upcase end
-  defp string_for_conv_spec(dt, :r, _) do strftime! dt, "%I:%M:%S %p" end
-  defp string_for_conv_spec(dt, :R, _) do strftime! dt, "%H:%M" end
-  defp string_for_conv_spec(dt, :T, _) do strftime! dt, "%H:%M:%S" end
-  defp string_for_conv_spec(dt, :F, _) do strftime! dt, "%Y-%m-%d" end
-  defp string_for_conv_spec(dt, :v, _) do strftime! dt, "%e-%b-%Y" end
-  defp string_for_conv_spec(dt, :m, _) do dt=to_date(dt);"#{dt.month}"|>pad end
-  defp string_for_conv_spec(dt, :e, _) do dt=to_date(dt);"#{dt.day}"|>pad(2, hd ' ') end
-  defp string_for_conv_spec(dt, :d, _) do dt=to_date(dt);"#{dt.day}"|>pad end
-  defp string_for_conv_spec(dt, :H, _) do dt=to_time(dt);"#{dt.hour}"|>pad end
-  defp string_for_conv_spec(dt, :k, _) do dt=to_time(dt);"#{dt.hour}"|>pad(2, hd ' ') end
-  defp string_for_conv_spec(dt, :M, _) do dt=to_time(dt);"#{dt.minute}"|>pad end
-  defp string_for_conv_spec(dt, :S, _) do dt=to_time(dt);"#{dt.second}"|>pad end
-  defp string_for_conv_spec(dt, :z, _) do z_offset_part(dt) end
-  defp string_for_conv_spec(dt, :Z, _) do "#{dt.zone_abbr}" end
+  defp string_for_conv_spec(dt, :a, lang) do
+    weekday_abbr(dt, lang)
+  end
 
-  defp string_for_conv_spec(dt, :x, lang), do: strftime! dt, date_format_for_lang(lang), lang
-  defp string_for_conv_spec(dt, :X, lang), do: strftime! dt, time_format_for_lang(lang), lang
-  defp string_for_conv_spec(dt, :c, lang), do: strftime! dt, date_time_format_for_lang(lang), lang
+  defp string_for_conv_spec(dt, :A, lang) do
+    weekday(dt, lang)
+  end
+
+  defp string_for_conv_spec(dt, :b, lang) do
+    month_abbr(dt, lang)
+  end
+
+  defp string_for_conv_spec(dt, :h, lang) do
+    string_for_conv_spec(dt, :b, lang)
+  end
+
+  defp string_for_conv_spec(dt, :B, lang) do
+    month(dt, lang)
+  end
+
+  defp string_for_conv_spec(dt, :f, _) do
+    micro_seconds(dt)
+  end
+
+  defp string_for_conv_spec(dt, :j, _) do
+    "#{day_number_in_year(dt)}" |> pad(3)
+  end
+
+  defp string_for_conv_spec(dt, :u, _) do
+    "#{day_of_the_week(dt)}"
+  end
+
+  defp string_for_conv_spec(dt, :w, _) do
+    "#{day_of_the_week_zero_sunday(dt)}"
+  end
+
+  defp string_for_conv_spec(dt, :V, _) do
+    "#{elem(iso_week_number(dt), 1)}" |> pad
+  end
+
+  defp string_for_conv_spec(dt, :G, _) do
+    "#{elem(iso_week_number(dt), 0)}" |> pad(4)
+  end
+
+  defp string_for_conv_spec(dt, :g, _) do
+    string_for_conv_spec(dt, :G, nil) |> String.slice(-2..-1)
+  end
+
+  defp string_for_conv_spec(dt, :y, _) do
+    dt = to_date(dt)
+    "#{dt.year}" |> String.slice(-2..-1)
+  end
+
+  defp string_for_conv_spec(dt, :Y, _) do
+    dt = to_date(dt)
+    "#{dt.year}" |> pad(4)
+  end
+
+  defp string_for_conv_spec(dt, :C, _) do
+    dt = to_date(dt)
+    "#{(dt.year / 100.0) |> trunc}"
+  end
+
+  defp string_for_conv_spec(dt, :I, _) do
+    dt = to_time(dt)
+    "#{dt.hour |> x24h_to_12_h |> elem(0)}" |> pad
+  end
+
+  defp string_for_conv_spec(dt, :l, _) do
+    dt = to_time(dt)
+    "#{dt.hour |> x24h_to_12_h |> elem(0)}" |> pad(2, hd(' '))
+  end
+
+  defp string_for_conv_spec(dt, :P, _) do
+    dt = to_time(dt)
+    "#{dt.hour |> x24h_to_12_h |> elem(1)}"
+  end
+
+  defp string_for_conv_spec(dt, :p, _) do
+    dt = to_time(dt)
+    "#{dt.hour |> x24h_to_12_h |> elem(1)}" |> String.upcase()
+  end
+
+  defp string_for_conv_spec(dt, :r, _) do
+    strftime!(dt, "%I:%M:%S %p")
+  end
+
+  defp string_for_conv_spec(dt, :R, _) do
+    strftime!(dt, "%H:%M")
+  end
+
+  defp string_for_conv_spec(dt, :T, _) do
+    strftime!(dt, "%H:%M:%S")
+  end
+
+  defp string_for_conv_spec(dt, :F, _) do
+    strftime!(dt, "%Y-%m-%d")
+  end
+
+  defp string_for_conv_spec(dt, :v, _) do
+    strftime!(dt, "%e-%b-%Y")
+  end
+
+  defp string_for_conv_spec(dt, :m, _) do
+    dt = to_date(dt)
+    "#{dt.month}" |> pad
+  end
+
+  defp string_for_conv_spec(dt, :e, _) do
+    dt = to_date(dt)
+    "#{dt.day}" |> pad(2, hd(' '))
+  end
+
+  defp string_for_conv_spec(dt, :d, _) do
+    dt = to_date(dt)
+    "#{dt.day}" |> pad
+  end
+
+  defp string_for_conv_spec(dt, :H, _) do
+    dt = to_time(dt)
+    "#{dt.hour}" |> pad
+  end
+
+  defp string_for_conv_spec(dt, :k, _) do
+    dt = to_time(dt)
+    "#{dt.hour}" |> pad(2, hd(' '))
+  end
+
+  defp string_for_conv_spec(dt, :M, _) do
+    dt = to_time(dt)
+    "#{dt.minute}" |> pad
+  end
+
+  defp string_for_conv_spec(dt, :S, _) do
+    dt = to_time(dt)
+    "#{dt.second}" |> pad
+  end
+
+  defp string_for_conv_spec(dt, :z, _) do
+    z_offset_part(dt)
+  end
+
+  defp string_for_conv_spec(dt, :Z, _) do
+    "#{dt.zone_abbr}"
+  end
+
+  defp string_for_conv_spec(dt, :x, lang), do: strftime!(dt, date_format_for_lang(lang), lang)
+  defp string_for_conv_spec(dt, :X, lang), do: strftime!(dt, time_format_for_lang(lang), lang)
+
+  defp string_for_conv_spec(dt, :c, lang),
+    do: strftime!(dt, date_time_format_for_lang(lang), lang)
 
   defp micro_seconds(dt), do: "#{elem(dt.microsecond, 0)}"
 
@@ -179,47 +294,58 @@ defmodule Calendar.Strftime do
     total_off = dt.utc_offset + dt.std_offset
     sign = sign_for_offset(total_off)
     offset_amount_string = total_off |> secs_to_hours_mins_string
-    sign<>offset_amount_string
+    sign <> offset_amount_string
   end
+
   defp sign_for_offset(offset) when offset < 0, do: "-"
   defp sign_for_offset(_), do: "+"
+
   defp secs_to_hours_mins_string(secs) do
     secs = abs(secs)
-    hours = secs/3600.0 |> Float.floor |> trunc
-    mins = rem(secs, 3600)/60.0 |> Float.floor |> trunc
-    "#{hours|>pad(2)}#{mins|>pad(2)}"
+    hours = (secs / 3600.0) |> Float.floor() |> trunc
+    mins = (rem(secs, 3600) / 60.0) |> Float.floor() |> trunc
+    "#{hours |> pad(2)}#{mins |> pad(2)}"
   end
 
   defp weekday_abbr(dt, lang) do
     Enum.fetch!(weekday_names_abbr(lang), day_of_the_week(dt) - 1)
   end
+
   defp weekday(dt, lang) do
     Enum.fetch!(weekday_names(lang), day_of_the_week(dt) - 1)
   end
 
   defp month_abbr(dt, lang) do
     dt = dt |> to_date
-    Enum.fetch!(month_names_abbr(lang), dt.month-1)
+    Enum.fetch!(month_names_abbr(lang), dt.month - 1)
   end
+
   defp month(dt, lang) do
     dt = dt |> to_date
-    Enum.fetch!(month_names(lang), dt.month-1)
+    Enum.fetch!(month_names(lang), dt.month - 1)
   end
 
   defp iso_week_number(dt) do
-    dt |> Calendar.Date.week_number
+    dt |> Calendar.Date.week_number()
   end
 
   defp day_number_in_year(dt) do
-    dt |> Calendar.Date.day_number_in_year
+    dt |> Calendar.Date.day_number_in_year()
   end
 
-  defp day_of_the_week_zero_sunday(dt) do # sunday is 0
+  # sunday is 0
+  defp day_of_the_week_zero_sunday(dt) do
     dt |> day_of_the_week |> rem(7)
   end
 
-  defp x24h_to_12_h(0) do {12, :am} end
-  defp x24h_to_12_h(12) do {12, :pm} end
+  defp x24h_to_12_h(0) do
+    {12, :am}
+  end
+
+  defp x24h_to_12_h(12) do
+    {12, :pm}
+  end
+
   defp x24h_to_12_h(hour) when hour >= 1 and hour < 12, do: {hour, :am}
   defp x24h_to_12_h(hour) when hour > 12, do: {hour - 12, :pm}
 
@@ -232,55 +358,79 @@ defmodule Calendar.Strftime do
     {:ok, data} = translation_module().weekday_names(lang)
     data
   end
+
   defp weekday_names_abbr(lang) do
     {:ok, data} = translation_module().weekday_names_abbr(lang)
     data
   end
+
   defp month_names(lang) do
     {:ok, data} = translation_module().month_names(lang)
     data
   end
+
   defp month_names_abbr(lang) do
     {:ok, data} = translation_module().month_names_abbr(lang)
     data
   end
+
   defp date_format_for_lang(lang) do
     {:ok, data} = translation_module().date_format(lang)
     data
   end
+
   defp time_format_for_lang(lang) do
     {:ok, data} = translation_module().time_format(lang)
     data
   end
+
   defp date_time_format_for_lang(lang) do
     {:ok, data} = translation_module().date_time_format(lang)
     data
   end
+
   defp translation_module do
-    fetch_result =  Application.fetch_env(:calendar, :translation_module)
-    trans_mod = case fetch_result do
-      :error ->  Calendar.DefaultTranslations
-      {:ok, configured_module} -> configured_module
-    end
+    fetch_result = Application.fetch_env(:calendar, :translation_module)
+
+    trans_mod =
+      case fetch_result do
+        :error -> Calendar.DefaultTranslations
+        {:ok, configured_module} -> configured_module
+      end
+
     trans_mod
   end
 end
 
 defmodule Calendar.DefaultTranslations do
   def weekday_names(:en) do
-    {:ok, ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"] }
+    {:ok, ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]}
   end
 
   def weekday_names_abbr(:en) do
-    {:ok, ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"] }
+    {:ok, ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]}
   end
 
   def month_names(:en) do
-    {:ok, ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"] }
+    {:ok,
+     [
+       "January",
+       "February",
+       "March",
+       "April",
+       "May",
+       "June",
+       "July",
+       "August",
+       "September",
+       "October",
+       "November",
+       "December"
+     ]}
   end
 
   def month_names_abbr(:en) do
-    {:ok, ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"] }
+    {:ok, ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]}
   end
 
   def date_format(:en), do: {:ok, "%Y-%m-%d"}
